f30d1d192dac60b12ec352b8027465f59bd0c91f
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2026-04-14 22:12:58 +0530

    cors

--- server/src/app/app.ts
+++ server/src/app/app.ts
@@ -1,5 +1,6 @@
 import express, { urlencoded } from "express";
 import type { Express, Request, Response } from "express";
+import cors from "cors";
 import { authenticationMiddleware } from "./middleware/auth.middleware";
 
 import { authRouter } from "./auth/auth.routes";
@@ -14,6 +15,13 @@ import { setupSeatJobs } from "./queue/seat.scheduler";
 
 export function createApplication(): Express {
   const app = express();
+
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    }),
+  );
 
   app.use(express.json());
   app.use(urlencoded({ extended: true, limit: "5mb" }));
